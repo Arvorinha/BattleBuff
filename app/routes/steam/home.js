4a558cd03132bd5b4da26fa40fcf87ebc1b03f33
Matheus Roberto J. Batista <matheus_roberto_batista@hotmail.com>
2018-08-27 13:29:48 -0300

    Descomentado

--- app/routes/steam/home.js
+++ app/routes/steam/home.js
@@ -1,8 +1,7 @@
 module.exports = function(app){
   app.get('/', function(req,res){
     var pool = app.config.dbConnection;
-    //var steam = app.app.model.jogadorModel;
-    console.log(app.app.model.jogadorModel);
+    var steam = app.app.model.jogadorModel;
     steam.findAll(pool, function(err,result){
       if(err){
         console.log(err.stack);
