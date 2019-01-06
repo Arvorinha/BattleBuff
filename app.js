9a9390ddc5999ffb4650c5d183e4e15a0d3ed893
Breno Nunes Galvão <breno.nunesgalvao@gmail.com>
2019-01-06 03:15:05 -0200

    bugfix

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ var app = require('./config/server');
 //var home = require('./app/routes/steam/home')(app);
 //var home = require('./app/routes/steam/auth')(app);
 //var home = require('./app/routes/steam/verify')(app);
-var port = process.env.PORT || 5000
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.app.controller.sala.salaID(io, app);
 
-http.listen(port, function() {
-  console.log(`Servidor Rodando na porta ${ port }`);
+http.listen(port, ip, function() {
+  console.log('Server running on http://%s:%s', ip, port);
 });
 // app.listen(port, () => console.log(`Servidor Rodando na porta ${ port }`));
+// app.listen(port, ip);
+// console.log('Server running on http://%s:%s', ip, port);
